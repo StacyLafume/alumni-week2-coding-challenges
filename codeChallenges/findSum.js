14da0bf1d643ae34ef562e123941085f795bc63a
StacyLafume <stacylafume123@gmail.com>
2020-11-12 20:59:16 -0500

    findSum.js

---
+++ codeChallenges/findSum.js
@@ -0,0 +1,24 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples
+// GetSum(1, 0) == 1   // 1 + 0 = 1
+// GetSum(1, 2) == 3   // 1 + 2 = 3
+// GetSum(0, 1) == 1   // 0 + 1 = 1
+// GetSum(1, 1) == 1   // 1 Since both are same
+// GetSum(-1, 0) == -1 // -1 + 0 = -1
+// GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+
+
+
+const getSum = ( a,b ) => {
+    if(a===b)return b
+    let result = 0;
+    let smaller = Math.min(a,b)
+    const larger = Math.max(a,b)
+    for (let i = smaller; i <= larger; i ++ ){
+     result += i
+    }
+    return result
+  } 
